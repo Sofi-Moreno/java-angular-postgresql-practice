e58eaf18c003f1fdb8cf67b71dab4d4efdf86505
Sofia Moreno <sgmoreno.23@est.ucab.edu.ve>
2025-11-04 23:20:12 -0400

    Cuarto commit

--- src/main/java/com/example/demo_angular/controller/PeopleController.java
+++ src/main/java/com/example/demo_angular/controller/PeopleController.java
@@ -1,10 +1,19 @@
 package com.example.demo_angular.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo_angular.model.People;
+import com.example.demo_angular.model.ServiceResponse;
 import com.example.demo_angular.service.IPeopleService;
 
 @RestController
